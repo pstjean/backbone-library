2a6f6fa397a771407f3a954aa94089c307d8923a
Peter St.Jean <pstjean@gmail.com>
2014-01-22 13:26:42 -0600

    Adding and deleting books (with no persistence)

--- js/views/book.js
+++ js/views/book.js
@@ -4,6 +4,17 @@ app.BookView = Backbone.View.extend({
     tagName: 'div',
     className: 'bookContainer',
     template: _.template($('#bookTemplate').html()),
+    events: {
+        'click .delete': 'deleteBook'
+    },
+
+    deleteBook: function() {
+        // Delete model
+        this.model.destroy();
+
+        // Delete view
+        this.remove();
+    },
     render: function() {
         // this.el is what we defined in tagName. use $el to get access to jQuery html() function
         this.$el.html(this.template(this.model.toJSON()));
--- js/views/library.js
+++ js/views/library.js
@@ -5,6 +5,31 @@ app.LibraryView = Backbone.View.extend({
     initialize: function(initialBooks) {
         this.collection = new app.Library(initialBooks);
         this.render();
+
+        // make the view render again when a new model is added
+        this.listenTo( this.collection, 'add', this.renderBook );
+    },
+
+    events: {
+        'click #add': 'addBook'
+    },
+    addBook: function(e){
+        e.preventDefault();
+
+        var formData = {};
+
+        // We select all the input elements of the form that have a value and iterate over
+        // them using jQuery’s each. Since we used the same names for ids in our form as the
+        // keys on our Book model we can simply store them directly in the formData object.
+        $('#addBook div').children('input').each(function(i,el) {
+            // We skip fields without a value so that the defaults will be applied.
+            if($(el).val() != '') {
+                formData[el.id] = $(el).val();
+            }
+        });
+
+        //console.log(formData);
+        this.collection.add(new app.Book(formData));
     },
 
     // render library by rendering each book in its collection
